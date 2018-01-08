79e9dbc535c39bde20afd09a8f89d6262d73ae23
zhs007 <sssxueren@gmail.com>
2018-01-08 23:42:49 +0800

    upd

--- shell_sinastocktoday.js
+++ shell_sinastocktoday.js
@@ -3,6 +3,9 @@ log('info', 'exec sinastocktoday...');
 if (shell.which('node')) {
     log('info', 'node ./bin/sinastocktoday.js');
     shell.exec("node ./bin/sinastocktoday.js");
+
+    log('info', 're node ./bin/sinastocktoday.js');
+    shell.exec("node ./bin/sinastocktoday.js");
 }
 
 log('info', 'exec sinastocktoday end.');
